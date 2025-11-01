fca24aa0e6fe929dabc0a753caf28bafa69b2f95
Larissa Paulino <larissaspaulino@gmail.com>
2025-11-01 15:35:03 -0300

    fix NAT Gateway - per AZ

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -12,19 +12,26 @@ resource "aws_eip" "vpc_eip_1b" {
   }
 }
 
+resource "aws_eip" "vpc_eip_1c" {
+  domain = "vpc"
+  tags = {
+    Name = format("%s-eip-1c", var.project_name)
+  }
+}
+
+resource "aws_nat_gateway" "nat_1a" {
+  allocation_id = aws_eip.vpc_eip_1a.id
+  subnet_id     = aws_subnet.public_subnet_1a.id
+  tags = {
+    Name = format("%s-nat-1a", var.project_name)
+  }
+}
+
 resource "aws_nat_gateway" "nat_1b" {
   allocation_id = aws_eip.vpc_eip_1b.id
   subnet_id     = aws_subnet.public_subnet_1b.id
   tags = {
     Name = format("%s-nat-1b", var.project_name)
-  }
-}
-
-
-resource "aws_eip" "vpc_eip_1c" {
-  domain = "vpc"
-  tags = {
-    Name = format("%s-eip-1c", var.project_name)
   }
 }
 
