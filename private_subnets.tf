7e68eba0906d8c7dca219ab21ec86a58ba7afc8f
Larissa Paulino <larissaspaulino@gmail.com>
2025-11-01 15:22:54 -0300

    Route table for private subnet - per AZ

--- private_subnets.tf
+++ private_subnets.tf
@@ -32,9 +32,23 @@ resource "aws_subnet" "private_subnet_1c" {
 }
 
 # ROUTE TABLE
-resource "aws_route_table" "private_internet_access" {
+resource "aws_route_table" "private_internet_access_1a" {
   vpc_id = aws_vpc.main.id
   tags = {
-    Name = format("%s-private", var.project_name)
+    Name = format("%s-private-1a", var.project_name)
+  }
+}
+
+resource "aws_route_table" "private_internet_access_1b" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = format("%s-private-1b", var.project_name)
+  }
+}
+
+resource "aws_route_table" "private_internet_access_1c" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = format("%s-private-1c", var.project_name)
   }
 }
