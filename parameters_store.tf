cc322848377993e3cd0a2a58c3eac79253e91df5
Larissa Paulino <larissaspaulino@gmail.com>
2025-11-08 16:52:54 -0300

    databases subnets in parameters_store.tf

--- parameters_store.tf
+++ parameters_store.tf
@@ -40,3 +40,20 @@ resource "aws_ssm_parameter" "public_1c" {
   value = aws_subnet.public_subnet_1c.id
 }
 
+resource "aws_ssm_parameter" "databases_1a" {
+  name  = format("/%s/vpc/subnet_databases_1a", var.project_name)
+  type  = "String"
+  value = aws_subnet.databases_subnet_1a.id
+}
+
+resource "aws_ssm_parameter" "databases_1b" {
+  name  = format("/%s/vpc/subnet_databases_1b", var.project_name)
+  type  = "String"
+  value = aws_subnet.databases_subnet_1b.id
+}
+
+resource "aws_ssm_parameter" "databases_1c" {
+  name  = format("/%s/vpc/subnet_databases_1c", var.project_name)
+  type  = "String"
+  value = aws_subnet.databases_subnet_1c.id
+}
